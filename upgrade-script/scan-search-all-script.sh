ecc06dbc75062a47236953e900e393b6655bae04
Euiyoung Hwang <euiyoung.hwang@gxo.com>
2026-09-08 13:23:48 -0500

    Update

--- upgrade-script/scan-search-all-script.sh
+++ upgrade-script/scan-search-all-script.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 
-
 SCRIPTDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-cd $SCRIPTDIR
 
-echo "Running Path .. $SCRIPTDIR"
+PARENT_DIR=$(dirname "$SCRIPTDIR")
+
+echo "Running Path .. $PARENT_DIR"
+cd $PARENT_DIR
 
 VENV=".venv"
 
@@ -19,12 +20,24 @@ fi
 
 echo "Retrieve all records from ESv5 or ESv8"
 
-source_es_cluster="http://localhost:9200"
-target_es_cluster="https://localhost:9200"
-param_index="test"
+# source_es_cluster="http://localhost:9200"
+# target_es_cluster="https://localhost:9200"
+# param_index="test"
+
+source_es_cluster=$1
+target_es_cluster=$2
+param_index=$3
+
+# Run
+# Change .env for the authenication
+# ./upgrade-script/scan-search-all-script.sh http://localhost:9200  https://localhost:9200 test
 
 # env
 echo "Retrive all records from ESv5 ($source_es_cluster)"
 python ./upgrade-script/scan-search-all-script.py --es $source_es_cluster --index $param_index --version ESv5
 echo "Retrive all records from ESv8 ($target_es_cluster)"
-python ./upgrade-script/scan-search-all-script.py --es $target_es_cluster --index $param_index --version ESv5
+python ./upgrade-script/scan-search-all-script.py --es $target_es_cluster --index $param_index --version ESv8
+
+
+# Then,
+# python ./upgrade-script/scan-ids-compare-script
